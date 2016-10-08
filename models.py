dd2c9a860340f890b80a4da4ff387ca28165e1b0
Richard Lander <richard.lander@tribridge.com>
2016-10-08 17:19:54 -0400

    Create basic, working API using SQL database

---
+++ models.py
@@ -0,0 +1,106 @@
+import datetime
+
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Text, Boolean
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.schema import UniqueConstraint
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.dialects.postgresql import JSONB, HSTORE
+
+
+Base = declarative_base()
+
+
+class JurisdictionType(Base):
+    """
+    JurisdictionType allows for the definition and description of standard types
+    of Jurisdiction objects. The JurisdictionType objects define the hierarchy
+    that derived Jurisdiction objects will use.
+    """
+    __tablename__ = "jurisdiction_type"
+
+    id          = Column(Integer, primary_key=True, autoincrement=True)
+    name        = Column(Text, nullable=False)
+    description = Column(Text, nullable=False)
+    parent_id   = Column(Integer, ForeignKey('jurisdiction_type.id'), default=None)
+
+    jurisdiction_type = relationship('JurisdictionType',
+                                     backref=backref('child_jurisdiction_types', remote_side=[id]),
+                                     foreign_keys=parent_id)
+
+    def __attributes__(self):
+        return {
+            'id':          self.id,
+            'name':        self.name,
+            'description': self.description,
+            'parent_id':   self.parent_id
+        }
+
+
+class ConfigurationTemplate(Base):
+    """
+    A ConfigurationTemplate allows for the definition of standard configurations
+    for Jurisdictions.  ConfigurationTemplate objects are assigned to particular
+    JurisdictionType objects and Jurisdiction objects that are of that type can
+    have the standard configurations assigned when instantiated.
+    """
+    __tablename__ = "configuration_template"
+    id                   = Column(Integer, primary_key=True, autoincrement=True)
+    name                 = Column(Text, nullable=False)
+    configuration        = Column(JSONB, nullable=False)
+    default              = Column(Boolean, default=False)
+    jurisdiction_type_id = Column(Integer, ForeignKey('jurisdiction_type.id'),
+                                     default=None)
+
+    jurisdiction_type = relationship('JurisdictionType', backref='configurations',
+                                     foreign_keys=jurisdiction_type_id)
+
+    unique_default = UniqueConstraint(default, jurisdiction_type_id)
+
+    def __attributes__(self):
+        return {
+            'id':                   self.id,
+            'name':                 self.name,
+            'configuration':        self.configuration,
+            'default':              self.default,
+            'jurisdiction_type_id': self.jurisdiction_type_id
+        }
+
+
+class Jurisdiction(Base):
+    """
+    A Jurisdiction object represents a scope of authority for a group of
+    infrastrural resources. Jurisdiction objects can be nested within other
+    Jurisdictions of a different JurisdictionType.
+    """
+    __tablename__ = "jurisdiction"
+
+    id                    = Column(Integer, primary_key=True, autoincrement=True)
+    name                  = Column(Text, nullable=False, unique=True)
+    created_on            = Column(DateTime(timezone=True), default=datetime.datetime.utcnow())
+    active                = Column(Boolean, default=False)
+    configuration         = Column(JSONB, nullable=False)
+    assets                = Column(JSONB)
+    jurisdiction_metadata = Column(HSTORE)
+    jurisdiction_type_id  = Column(Integer, ForeignKey('jurisdiction_type.id'),
+                                  nullable=False)
+    parent_id             = Column(Integer, ForeignKey('jurisdiction.id'), default=None)
+
+    jurisdiction_type = relationship('JurisdictionType', backref='jurisdictions',
+                                     foreign_keys=jurisdiction_type_id)
+    jurisdiction      = relationship('Jurisdiction',
+                                     backref=backref('child_jurisdictions', remote_side=[id]),
+                                     foreign_keys=parent_id )
+
+    def __attributes__(self):
+        return {
+            'id':                   self.id,
+            'name':                 self.name,
+            'created_on':           self.created_on,
+            'active':               self.active,
+            'assets':               self.assets,
+            'configuration':        self.configuration,
+            'metadata':             self.jurisdiction_metadata,
+            'jurisdiction_type_id': self.jurisdiction_type_id,
+            'parent_id':            self.parent_id
+        }
+
---
+++ provisioner.py
@@ -0,0 +1,129 @@
+import falcon
+import hug
+import sqlalchemy
+
+import database as db
+from models import JurisdictionType, Jurisdiction, ConfigurationTemplate
+
+
+def get_objects(obj, obj_id, session):
+
+    if obj_id:
+        query = session.query(obj).filter_by(id=obj_id)
+        if query.count() == 0:
+            msg = "{0} with id {1} does not exist".format(obj.__name__, obj_id)
+            raise falcon.HTTPBadRequest('Bad request', msg)
+    else:
+        query = session.query(obj).all()
+
+    return query
+
+
+def get_object_attributes(obj, obj_id, session):
+    
+    objects = get_objects(obj, obj_id, session)
+    
+    object_attributes = []
+    for o in objects:
+        object_attributes.append(o.__attributes__())
+
+    return object_attributes
+
+
+@hug.get('/get_jurisdiction_types/', version=1)
+def get_jurisdiction_types(jurisdiction_type_id: hug.types.number=None):
+
+    with db.transaction() as session:
+        jt_attrs = get_object_attributes(JurisdictionType,
+                                         jurisdiction_type_id,
+                                         session)
+
+    return jt_attrs
+
+
+@hug.get('/get_configuration_templates/', version=1)
+def get_configuration_templates(configuration_template_id: hug.types.number=None):
+
+    with db.transaction() as session:
+        ct_attrs = get_object_attributes(ConfigurationTemplate,
+                                         configuration_template_id,
+                                         session)
+
+    return ct_attrs
+
+
+@hug.get('/get_jurisdictions/', version=1)
+def get_jurisdictions(jurisdiction_id: hug.types.number=None):
+
+    with db.transaction() as session:
+        j_attrs = get_object_attributes(Jurisdiction,
+                                        jurisdiction_id,
+                                        session)
+
+    return j_attrs
+
+
+@hug.post('/create_jurisdiction/', version=1)
+def create_jurisdiction(jurisdiction_name: hug.types.text,
+                        jurisdiction_type_id: hug.types.number,
+                        configuration_template_id: hug.types.number,
+                        parent_id: hug.types.number=None):
+
+    with db.transaction() as session:
+        name_exists = session.query(sqlalchemy.sql.exists().where(
+                            Jurisdiction.name == jurisdiction_name)).scalar()
+        if name_exists:
+            msg = "Jurisdiction '{}' already exists".format(jurisdiction_name)
+            raise falcon.HTTPBadRequest('Bad request', msg)
+
+        jurisdiction_type = get_objects(JurisdictionType,
+                                        jurisdiction_type_id,
+                                        session)[0]        
+        configuration_template = get_objects(ConfigurationTemplate,
+                                             configuration_template_id,
+                                             session)[0]
+        if configuration_template.jurisdiction_type_id != jurisdiction_type.id:
+            msg = """
+                ConfigurationTemplate with id {0} is not a template for
+                JurisdictionType '{1}'
+            """.format(configuration_template_id, jurisdiction_type.name)
+            raise falcon.HTTPBadRequest('Bad request', ' '.join(msg.split()))
+        if parent_id:
+            parent = get_objects(Jurisdiction, parent_id, session)[0]
+
+        new_jurisdiction = Jurisdiction(name=jurisdiction_name,
+                                        jurisdiction_type_id=jurisdiction_type.id,
+                                        configuration=configuration_template.configuration,
+                                        parent_id=parent_id)
+        session.add(new_jurisdiction)
+
+    with db.transaction() as session:
+        jurisdiction = session.query(Jurisdiction).filter_by(name=jurisdiction_name)[0]
+        response = jurisdiction.__attributes__()
+
+    return response
+
+
+@hug.put('/edit_jurisdiction/', version=1)
+def edit_jurisdiction(jurisdiction_id: hug.types.number, **edits):
+
+    # need to change metadata key - sqlalchemy uses metadata as table attribute
+    if 'metadata' in edits:
+        edits['jurisdiction_metadata'] = edits.pop('metadata')
+
+    with db.transaction() as session:
+        jurisdiction = session.query(Jurisdiction).filter_by(id=jurisdiction_id)[0]
+
+        for attr in edits:
+            if attr in ('name', 'jurisdiction_metadata', 'configuration'):
+                jurisdiction.__setattr__(attr, edits[attr])
+            else:
+                msg = '{} is not a Jurisdiction attribute that can be edited'.format(attr)
+                raise falcon.HTTPBadRequest('Bad request', msg)
+
+    with db.transaction() as session:
+        jurisdiction = session.query(Jurisdiction).filter_by(id=jurisdiction_id)[0]
+        response = jurisdiction.__attributes__()
+
+    return response
+
